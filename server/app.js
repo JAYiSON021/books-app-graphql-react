1d06bacb908d24e2b7410a5ad830e5f2a24afb3b
Jayson San Agustin <sanagustinjayson@gmail.com>
2018-12-19 15:08:50 +0800

    Added database connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to mongodb
+mongoose.connect(`mongodb://localhost:27017/books`, { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+ console.log('Connected to database.');
+});
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
